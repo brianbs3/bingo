8d864e51ff2ea8881805b29f2ba297857bca1159
bsizemore <bs@home.bs>
2013-02-19 01:39:01 -0500

    starting

--- bingo.js
+++ bingo.js
@@ -1,3 +1,9 @@
+$(document).ready(function()
+{
+  console.log('ready');
+  $('#bingo').html('hello').css('background-color', 'red');
+});
+
 function print_board()
 {
   console.log('print board');
